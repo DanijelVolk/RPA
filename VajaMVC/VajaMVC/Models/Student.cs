9285df5eb33c1d3c492495e1d37a25ad39cd001a
Danijel Volk <danijel.volk.97@gmail.com>
2018-04-18 12:00:37 +0200

    prvi kommit.

---
+++ VajaMVC/VajaMVC/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VajaMVC.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Display(Name="Ime")]
+        [Required(ErrorMessage ="NAROBE NOOOOO... napačen podatek ime.")]
+        public string StudentName { get; set; }
+        [Display(Name = "Starost")]
+        [Required(ErrorMessage = "NAROBE NOOOOO... napačen podatek starosti.")]
+        [Range(18,50, ErrorMessage = "NAROBE NOOOOO... med 18 in 50.")]
+        public int Age { get; set; }
+
+
+    }
+}
