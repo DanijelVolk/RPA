32558fa86b1fb17f6ab8ca7c69c68ebe0680b7a0
Danijel Volk <danijel.volk.97@gmail.com>
2018-04-23 09:43:25 +0200

    spremenjeno dodajanje slik in drugačen prikaz

---
+++ VajaNajprejBaza/VajaNajprejBaza/Controllers/ProductsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VajaNajprejBaza.Models;
+
+namespace VajaNajprejBaza.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ADWEntities db = new ADWEntities();
+        //*********
+        public ActionResult Show(int id)
+        {
+            var imageData = db.Product.Where(p => p.ProductID == id).Select(p => p.ThumbNailPhoto).FirstOrDefault().ToArray();
+            return File(imageData,"image/jpg");
+        }
+        //*************
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var product = db.Product.Include(p => p.ProductCategory);
+            return View(product.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProductCategoryID = new SelectList(db.ProductCategory, "ProductCategoryID", "Name");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        //*******
+        public ActionResult Create([Bind(Include = "ProductID,Name,ProductNumber,Color,StandardCost,ListPrice,Size,Weight,ProductCategoryID," +
+            "ProductModelID,SellStartDate,SellEndDate,DiscontinuedDate,ThumbNailPhoto,ThumbnailPhotoFileName")] Product product, HttpPostedFileBase file)
+        {
+           
+            product.rowguid = Guid.NewGuid();
+            product.ModifiedDate = DateTime.Now;
+        //*********
+            if (ModelState.IsValid)
+            {
+                //******
+                if(file != null)
+                {
+                    string pic = Path.GetFileName(file.FileName); //pridobljeno ime dat.
+                    string pot = Path.Combine(Server.MapPath("~/images"), pic); //pot mape na strežniku
+                    file.SaveAs(pot); //naloži dat. na strežnik
+
+                    //shrani ime dat/slike v bazo
+                    FileInfo fi = new FileInfo(pot);
+                    product.ThumbnailPhotoFileName = fi.Name;
+                    //shrani sliko v bazo
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        file.InputStream.CopyTo(ms); //neko zaporedje bajtov - kao slika
+                        product.ThumbNailPhoto = ms.GetBuffer();
+                    }
+                }
+
+                //******
+                db.Product.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductCategoryID = new SelectList(db.ProductCategory, "ProductCategoryID", "Name", product.ProductCategoryID);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductCategoryID = new SelectList(db.ProductCategory, "ProductCategoryID", "Name", product.ProductCategoryID);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,Name,ProductNumber,Color,StandardCost,ListPrice,Size,Weight,ProductCategoryID,ProductModelID,SellStartDate,SellEndDate,DiscontinuedDate,ThumbNailPhoto,ThumbnailPhotoFileName,rowguid,ModifiedDate")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductCategoryID = new SelectList(db.ProductCategory, "ProductCategoryID", "Name", product.ProductCategoryID);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Product.Find(id);
+            db.Product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
